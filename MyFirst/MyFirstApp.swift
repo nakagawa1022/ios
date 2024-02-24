d0ebb3615f92058167cb2d22849745fab606a24c
nakagawa1022 <129717557+nakagawa1022@users.noreply.github.com>
2024-02-24 23:53:07 +0900

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by hiroki.nakagawa on 2024/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
